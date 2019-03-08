e723c9a4d3cad5d2e42f642ec429a41f3836dfa4
Jan <42362229+Magichanics@users.noreply.github.com>
2019-03-08 06:22:25 -0500

    made modifications for spreadsheet gui to run(wip)

--- wcg_gui.py
+++ wcg_gui.py
@@ -3,6 +3,18 @@ Name: Jan Garong
 Date: February 10th, 2019
 
 based off of example: https://www.blog.pythonlibrary.org/2011/08/20/wxpython-new-widget-announced-xlsgrid/
+
+
+------------------------------------------------------------------------------------------------------------------------
+
+Note: please modify zlsgrid.py at line 1975
+
+if not current == None:
+    col_width = int(round(float(default_width)*current/256.0))
+else:
+    col_width = 20
+
+------------------------------------------------------------------------------------------------------------------------
 '''
 import pandas as pd
 
@@ -20,13 +32,15 @@ class WCG_Form(wx.Frame):
         panel = wx.Panel(self, wx.ID_ANY)
 
         # access csv file
-        book = xlrd.open_workbook("assets/metadata.xml", formatting_info=1)
-        sheetname = "Works Cited Metadata"
-        sheet = book.sheet_by_name(sheetname)
+        book = xlrd.open_workbook("assets/metadata.xls", formatting_info=1)
+
+        # obtain sheet and sheet name
+        sheetname = book.sheet_names()[0]
+        sheet = book.sheet_by_name(book.sheet_names()[0])
 
         # determine dimensions
         rows, cols = sheet.nrows, sheet.ncols
-        comments, texts = XG.ReadExcelCOM(filename, sheetname, rows, cols)
+        comments, texts = XG.ReadExcelCOM("assets/metadata.xls", sheetname, rows, cols)
 
         xlsGrid = XG.XLSGrid(panel)
         xlsGrid.PopulateGrid(book, sheet, texts, comments)
